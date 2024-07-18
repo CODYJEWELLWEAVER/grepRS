3e91512430e2ea9a0425c42d46e42d5d2268378f
Cody Weaver <cowe7614@colorado.edu>
2024-07-18 15:58:49 -0600

    Add output buffering.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod options;
 pub mod output;
 
 use config::Config;
+use output::OutputBuffer;
+use source::Source;
 use regex::{Regex, Matches};
 use std::error::Error;
 use std::io::{stderr, Write};
@@ -20,6 +22,10 @@ pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
 
     let config: Config = Config::new(args)?;
     let regex: Regex = matcher::build_regex(&config.options)?;
+    let mut output_buffer = OutputBuffer::new();
+
+    let last_source_idx = config.sources.len() - 1;
+    let last_source: &Source = &config.sources[last_source_idx].clone();
 
     for mut source in config.sources {
         if let Err(msg) = source.read_content() {
@@ -38,8 +44,15 @@ pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
             &source.content
         );
 
-        output::display(&config.options, &source, matches);
+        output_buffer.write_to_buffer(&config.options, &source, matches);
+
+        // print source separator
+        if source.path != last_source.path {
+            output_buffer.write_separator();
+        }
     }
+
+    output_buffer.write_and_flush();
 
     return Ok(());
 }
--- src/output/mod.rs
+++ src/output/mod.rs
@@ -5,40 +5,72 @@ use source::Source;
 use options::Options;
 use regex::Matches;
 use std::iter::zip;
+use std::io::{stdout, Write};
 
-/// Displays results of searching a [Source].
-pub fn display(options: &Options, source: &Source, source_matches: Vec<Matches>) {
-    let source_lines = source.content.split("\n");
-    let line_matches = zip(source_lines, source_matches);
+const BUFFER_SIZE: usize = 2048;
 
-    for (line, matches) in line_matches {
-        let mut matches = matches.peekable();
-
-        if matches.peek().is_some() && !options.invert_match {
-            display_line(options, source, line);
-        }
-        else if matches.peek().is_none() && options.invert_match {
-            display_line(options, source, line);
-        }
-    }
-
-    println!();
+/// Contains methods for buffering and writing output.
+pub struct OutputBuffer {
+    pub buffer: String,
+    pub out: Box<dyn Write>,
 }
 
-/// Displays line of source.
-fn display_line(options: &Options, source: &Source, line: &str) {
-    let mut output = String::new();
-
-    if options.file_prefix {
-        let prefix = String::from(&source.path) + ":\t";
-        output.push_str(prefix.as_str());
+impl OutputBuffer {
+    pub fn new() -> OutputBuffer {
+        OutputBuffer {
+            buffer: String::with_capacity(BUFFER_SIZE),
+            out: Box::from(stdout()),
+        }
     }
 
-    output.push_str(line);
+    /// Displays results of searching a [Source].
+    pub fn write_to_buffer(
+        &mut self,
+        options: &Options,
+        source: &Source,
+        source_matches: Vec<Matches>
+    ) {
+        let source_lines = source.content.split("\n");
+        let line_matches = zip(source_lines, source_matches);
 
-    if !output.ends_with("\n") {
-        output.push_str("\n");
+        for (line, matches) in line_matches {
+            let mut matches = matches.peekable();
+
+            if matches.peek().is_some() && !options.invert_match {
+                self.write_line(options, source, line);
+            }
+            else if matches.peek().is_none() && options.invert_match {
+                self.write_line(options, source, line);
+            }
+        }
     }
 
-    print!("{}", output);
+    pub fn write_separator(&mut self) {
+        self.buffer.push('\n');
+    }
+
+    pub fn write_and_flush(&mut self) {
+        write!(
+            self.out,
+            "{}",
+            self.buffer
+        ).expect("grepRS: Could not write to stdout!");
+
+        self.out.flush()
+            .expect("grepRS: Could not flush buffer!");
+    }
+
+    // writes line to buffer
+    fn write_line(&mut self, options: &Options, source: &Source, line: &str) {
+        if options.file_prefix {
+            let prefix = String::from(&source.path) + ":\t";
+            self.buffer.push_str(prefix.as_str());
+        }
+
+        self.buffer.push_str(line);
+
+        if !self.buffer.ends_with("\n") {
+            self.buffer.push_str("\n");
+        }
+    }
 }
